bca7cfde95430f5eed077ab9395bba16f6fdb2e2
ankitakalgut <kalgutkarankita@yahoo.in>
2018-09-18 12:59:29 +0530

    HelloExample

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.main;
+
+/**
+ * @author Ankita Kalgutkar
+ *
+ * 18-May-2018
+
+ */
+public class HelloWorld
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello jenkins....");
+	}
+}
